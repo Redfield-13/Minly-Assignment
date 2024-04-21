14e4f582975cd4eb4311a4b32b8e44163f0c484e
Redfield-13 <fieldr62@gmail.com>
2024-04-21 05:37:26 +0200

    react native added

---
+++ Frontend/media_sharing/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'media_sharing',
+  webDir: 'build'
+};
+
+export default config;
